24854263ab27662dc6e361dee7269c1fdc6e3e9c
Diego Parra <diegoparraferreira@gmail.com>
2023-06-22 14:20:37 -0400

    feat: updates

--- src/config/config.go
+++ src/config/config.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -21,9 +23,9 @@ var (
 func Load() {
 	var err error
 
-	// if err = godotenv.Load(); err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 
 	Port, err = strconv.Atoi(os.Getenv("APP_PORT"))
 	if err != nil {
